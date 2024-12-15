9b009b68d1b8c932169443c7a29f825519e029bd
Yaroslav <lgalahad88@gmail.com>
2024-12-15 20:14:36 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let userNumbInput = prompt("Введіть ціле число");
+userNumbInput = userNumbInput?.trim();
+
+if (!userNumbInput || !Number.isInteger(Number(userNumbInput))) {
+    console.log("Error! Некоректне число. Введіть ціле число.");
+}
+else {
+    const userNumb = Number(userNumbInput);
+    
+    if (userNumb <= 1) {
+        console.log("не просте число");
+    }
+    else {
+        let isPrime = true;
+
+        for(let i = 2; i <= Math.sqrt(userNumb); i++) { 
+            if (userNumb % i === 0) {
+                isPrime = false; 
+                break;
+            }
+        }
+
+        if (isPrime) {
+            console.log("просте число");
+        } else {
+            console.log("не просте число");
+        }
+    }
+}
+
+
+
